1b2423c5249b4fd31feab5fa199e10c9f3999463
Marina Trevisoli <marinagervino@gmail.com>
2024-09-06 19:20:11 -0300

    dados.js

---
+++ dados.js
@@ -0,0 +1,15 @@
+// Dados de exemplo de projetos sociais
+const projetos = [
+    {
+        id: 1,
+        nome: "Projeto A",
+        descricao: "Descrição do Projeto A",
+        localizacao: [51.5, -0.09]
+    },
+    {
+        id: 2,
+        nome: "Projeto B",
+        descricao: "Descrição do Projeto B",
+        localizacao: [51.51, -0.1]
+    }
+];
