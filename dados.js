c076cb842a61fd129b5052e61571e9d849a9a593
Marina Trevisoli <marinagervino@gmail.com>
2024-09-06 20:07:11 -0300

    dados.js

--- dados.js
+++ dados.js
@@ -1,16 +1,44 @@
-// Dados de exemplo de projetos sociais
+// Dados de projetos sociais
 const projetos = [
     {
-        id: 1,
-        nome: "Projeto A",
-        descricao: "Descrição do Projeto A",
-        localizacao: [51.5, -0.09]
+        nome: "Programa Conservador da Mantiqueira",
+        descricao: "Um projeto da OSC The Nature Conservancy que visa recuperar a floresta da cadeia de montanhas que se estende entre os estados de Minas Gerais, Rio de Janeiro e São Paulo."
     },
     {
-        id: 2,
-        nome: "Projeto B",
-        descricao: "Descrição do Projeto B",
-        localizacao: [51.51, -0.1]
+        nome: "Fundação Clara Lionel",
+        descricao: "Criada pela cantora Rihanna em 2012, esta fundação oferece educação e saúde à população de Barbados, país de origem da cantora."
+    },
+    {
+        nome: "WWF",
+        descricao: "Esta organização atua em projetos de conservação da biodiversidade e dos recursos naturais renováveis, além de promover a redução da poluição e do desperdício."
+    },
+    {
+        nome: "Casa Ronald McDonald ABC",
+        descricao: "Uma ONG que atua na área de assistência social."
+    },
+    {
+        nome: "ChildFund Brasil",
+        descricao: "Uma ONG que atua na área de criança e adolescente."
+    },
+    {
+        nome: "SP Leituras",
+        descricao: "Uma ONG que atua na área de cultura."
+    },
+    {
+        nome: "Engenheiros Sem Fronteiras",
+        descricao: "Uma ONG que atua na área de desenvolvimento local."
+    },
+    {
+        nome: "Refúgio 343",
+        descricao: "Uma ONG que atua na área de direitos humanos."
+    },
+    {
+        nome: "Escoteiros do Brasil",
+        descricao: "Uma ONG que atua na área de educação."
+    },
+    {
+        nome: "Fundação Gol de Letra",
+        descricao: "Uma ONG que atua na área de esporte."
     }
 ];
 
